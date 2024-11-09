499eb31b25bffe2fbb9a72b14f0b9bea1ed05db9
Balungile <danca0717342125@gmail.com>
2024-11-10 00:37:41 +0200

    added dir

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
 
 def list_books(request):
     books = Book.objects.all() 
@@ -9,6 +9,7 @@ def list_books(request):
     }
     return render(request, 'relationship_app/list_books.html', context)
 
+from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
