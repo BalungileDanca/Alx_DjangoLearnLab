79bda633367aeca7edbc0c4c6b3985fbe89fd564
Balungile <danca0717342125@gmail.com>
2024-11-17 16:41:22 +0200

    added directory

--- advanced_features_and_security/Djangoproject/relationship_app/models.py
+++ advanced_features_and_security/Djangoproject/relationship_app/models.py
@@ -34,4 +34,22 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.role})'
+    
+
 # Create your models here.
